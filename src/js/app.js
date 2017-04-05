05e0e6ecd5fa0224c57ce9fc1b0c67e08971e05b
Marc Günnel <marc.guennel@adesso.de>
2017-04-05 23:38:53 +0200

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+var AppViewModel = function() {
+    
+    this.isSearchControl = ko.observable(true);
+    this.searchValue = ko.observable();
+    
+    
+    this.showSearchControl = function() {
+        this.isSearchControl(!this.isSearchControl());
+    };
+
+    this.search = function(formElement) {
+        // ajax(this.searchValue());
+        var searchUrl = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + this.searchValue() + '&key=AIzaSyAM-2JC_5a5CDvXU_mWH5exAUO9HC1mbhg';
+
+        $.ajax({
+            url: searchUrl,
+            dataType: 'jsonp',
+            success: function(data) {
+                console.log(data);
+            }
+        });
+    };
+};
+
+ko.applyBindings(new AppViewModel());
