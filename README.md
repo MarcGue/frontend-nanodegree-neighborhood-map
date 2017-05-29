c99fc72d64cfde163de61e02d0897f263e5ac79d
Marc Günnel <marc.guennel@adesso.de>
2017-05-29 12:30:52 +0200

    added stuff to fullfill project rubric

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # frontend-nanodegree-neighborhood-map
-Udacity Front-End Web Developer Nanodegree - Project Neighborhood Map
+> This is the Udacity Front-End Web Developer Nanodegree - Project Neighborhood Map.
+> It shows different shopping locations in Berlin.
+> You can filter for any location on the map.
+
+## Installation
+Download or clone this repository
+
+    https://github.com/MarcGue/frontend-nanodegree-neighborhood-map
+
+## How to use
+- You can filter the given locations by its name
+- By clicking an item on the list a infowindow with some information will show up on the map
+- By clicking a marker on the map a infowindow with some information will show up on the map
+
+## See live demo
+Open the following URL with a browser of your choice:
+    
+    https://marcgue.github.io/frontend-nanodegree-neighborhood-map/
+
+## Usage of API's
+- Google's Map API
+- FourSquare API
--- js/app.js
+++ js/app.js
@@ -1,19 +1,67 @@
 'use strict';
 
+// Client Id of the FourSquare API
 var fsquareClientId = 'CNOWHNKFA2JKEKZ12FCHNC1WRP2WTM2JKTXAEIVXQ1X24SBJ';
+// Client Secret of the FourSquare API
 var fsquareClientSecrect = '52DX2KBYRNGDGWJZ244TTCANOJVO3TRGIERMZ5KGE5OAEFP2';
-
-var initialLocations = [
-    {title: 'Gropius Passagen', location: {lat: 52.4295793, lng: 13.4546777}},
-    {title: 'Potsdamer Platz Arkaden', location: {lat: 52.5078631, lng: 13.3738804}},
-    {title: 'ALEXA', location: {lat: 52.519903, lng: 13.4147787}},
-    {title: 'LP12 Mall of Berlin', location: {lat: 52.5104228, lng: 13.3810472}},
-    {title: 'Kaufhaus des Westens', location: {lat: 52.5016021, lng: 13.340993}},
-    {title: 'Spandau Arcaden', location: {lat: 52.5339545, lng: 13.1962588}},
-    {title: 'Tempelhofer Hafen Berlin', location: {lat: 52.4555581, lng: 13.3858547}}
+// Static data of different locations
+var initialLocations = [{
+        title: 'Gropius Passagen',
+        location: {
+            lat: 52.4295793,
+            lng: 13.4546777
+        }
+    },
+    {
+        title: 'Potsdamer Platz Arkaden',
+        location: {
+            lat: 52.5078631,
+            lng: 13.3738804
+        }
+    },
+    {
+        title: 'ALEXA',
+        location: {
+            lat: 52.519903,
+            lng: 13.4147787
+        }
+    },
+    {
+        title: 'LP12 Mall of Berlin',
+        location: {
+            lat: 52.5104228,
+            lng: 13.3810472
+        }
+    },
+    {
+        title: 'Kaufhaus des Westens',
+        location: {
+            lat: 52.5016021,
+            lng: 13.340993
+        }
+    },
+    {
+        title: 'Spandau Arcaden',
+        location: {
+            lat: 52.5339545,
+            lng: 13.1962588
+        }
+    },
+    {
+        title: 'Tempelhofer Hafen Berlin',
+        location: {
+            lat: 52.4555581,
+            lng: 13.3858547
+        }
+    }
 ];
 
-
+/**
+ * Location model that will call FourSquare API to get some additional information
+ * @param {*} locationData
+ * @param {*} map
+ * @param {*} infowindow
+ */
 function Location(locationData, map, infowindow) {
     // Store this to self
     var self = this;
@@ -34,12 +82,12 @@ function Location(locationData, map, infowindow) {
     // Build the url for making an ajax call to FourSquare
     var fsquareUrl = getFSquareUrl(self.title, self.position);
     // Make an ajax call to FourSquare about the location
-    $.getJSON(fsquareUrl).done(function(data) {
+    $.getJSON(fsquareUrl).done(function (data) {
         var firstResult = data.response.venues[0];
         self.url = firstResult.url;
         self.street = firstResult.location.formattedAddress[0];
         self.zipcode = firstResult.location.formattedAddress[1];
-    }).fail(function() {
+    }).fail(function () {
         alert('Error on calling FourSquare API');
     });
     // Create a marker with the title and position
@@ -51,30 +99,37 @@ function Location(locationData, map, infowindow) {
     // Set the map to the marker
     self.marker.setMap(self.map);
     // Add click listener to the marker
-    self.marker.addListener('click', function() {
+    self.marker.addListener('click', function () {
         populateInfowindow(self);
         bounceMarker(self);
     });
 }
 
+/**
+ * The KnockoutJS AppViewModel
+ */
 function AppViewModel() {
     var self = this;
 
+    // Create a map with Berlin as center
     self.map = new google.maps.Map(document.getElementById('map'), {
         zoom: 11,
-        center: {lat: 52.520008, lng: 13.404954}
-	});
-    self.geocoder = new google.maps.Geocoder();
+        center: {
+            lat: 52.520008,
+            lng: 13.404954
+        }
+    });
+    // Create a infowindow
     self.infowindow = new google.maps.InfoWindow();
-    self.placesService = new google.maps.places.PlacesService(self.map);
-
+    // Store the value of the filter
     self.filterValue = ko.observable('');
-
+    // Store all locations
     self.locations = ko.observableArray();
-    initialLocations.forEach(function(initialLocation) {
+    // Create and store all locations from the initalLocations
+    initialLocations.forEach(function (initialLocation) {
         self.locations.push(new Location(initialLocation, self.map, self.infowindow));
     });
-
+    // Create a computed field to filter the locations
     self.filteredLocations = ko.computed(function () {
         if (!self.filterValue()) {
             self.locations().forEach(function (loc) {
@@ -97,20 +152,33 @@ function AppViewModel() {
             return results;
         }
     });
-
-    self.highlightMarker = function(location, event) {
+    
+    /**
+     * Function to call the click event of a Marker in the map
+     */
+    self.highlightMarker = function (location, event) {
         if (event !== undefined && 'click' === event.type) {
             google.maps.event.trigger(location.marker, 'click');
         }
     };
 }
 
+/**
+ * Creates the complete URL to make a FourSquare API request
+ * @param {string} title 
+ * @param {array} position 
+ * @return the complete URL to make a FourSquare API request
+ */
 function getFSquareUrl(title, position) {
     var lat = position.lat;
     var lng = position.lng;
-    return 'https://api.foursquare.com/v2/venues/search?ll='+ lat + ',' + lng + '&client_id=' + fsquareClientId + '&client_secret=' + fsquareClientSecrect + '&v=20170525&query=' + title;
+    return 'https://api.foursquare.com/v2/venues/search?ll=' + lat + ',' + lng + '&client_id=' + fsquareClientId + '&client_secret=' + fsquareClientSecrect + '&v=20170525&query=' + title;
 }
 
+/**
+ * Set the information to the infowindow
+ * @param {Location} location 
+ */
 function populateInfowindow(location) {
     if (location.infowindow.marker != location.marker) {
         // Clear the content of the infowindow
@@ -118,30 +186,38 @@ function populateInfowindow(location) {
         // Set the new marker to the infowindow
         location.infowindow.marker = location.marker;
         // Add closeclick listener to the infowindow
-        location.infowindow.addListener('closeclick', function() {
+        location.infowindow.addListener('closeclick', function () {
             location.infowindow.marker = null;
         });
         // Set the content to the infowindow
-        location.infowindow.setContent('<h5>' + location.title + '</h5>' + 
-        '<a href="' + location.url + '">' + location.url + '</a>' + 
-        '<div>' + location.street + '</div>' +
-        '<div>' + location.zipcode + '</div>');
+        location.infowindow.setContent('<h5>' + location.title + '</h5>' +
+            '<a href="' + location.url + '">' + location.url + '</a>' +
+            '<div>' + location.street + '</div>' +
+            '<div>' + location.zipcode + '</div>' +
+            '<hr><small>powered by FourSquare</small>');
         // Open the infowindow on the correct marker
         location.infowindow.open(location.map, location.marker);
     }
 }
 
+/**
+ * Set the animation to the Marker of the given Location
+ * @param {Location} location 
+ */
 function bounceMarker(location) {
     if (location.marker.getAnimation() !== null) {
         location.marker.setAnimation(null);
     } else {
         location.marker.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function() {
+        setTimeout(function () {
             location.marker.setAnimation(null);
         }, 2500);
     }
 }
 
+/**
+ * Main method to start the app
+ */
 function initMap() {
     ko.applyBindings(new AppViewModel());
 }
